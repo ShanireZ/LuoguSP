c9c76ee613f5a85a083806557f03251551eacfe6
ShanireZ <shanire@skylinejoy.com>
2026-07-26 20:47:54 +0800

    feat: 更新版本号至 2.13.1，优化 CDN 运行时错误提示

--- LuoguSP.user.js
+++ LuoguSP.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LuoguSP
 // @namespace    https://github.com/ShanireZ/LuoguSP
-// @version      2.13.0
+// @version      2.13.1
 // @description  LuoguSP：题号显示难度颜色 / 私信 Ctrl+Click 打开用户个人页 / 个人页显示个人介绍 / IDE 模式一键测试所有样例 / 显示受限文章与剪贴板
 // @author       ShanireZ, realskc (Until 1.8.2)
 // @license      GPL-3.0
@@ -19,4 +19,10 @@
 // @require      https://spcdn.betaoi.cn/releases/2.13.0/compat/runtime.dad6604731c7e837.js#sha256=dad6604731c7e837d932527d78c4f13aa5cfe85d60adbb29ff6ea76d570988e0
 // @run-at       document-start
 // ==/UserScript==
-(()=>{var i=globalThis.__LUOGUSP_CDN_RUNTIME__;(!i||i.apiVersion!==1)&&console.error("LuoguSP CDN runtime was not initialized. Please update or reinstall LuoguSP.");})();
+(() => {
+  var i = globalThis.__LUOGUSP_CDN_RUNTIME__;
+  (!i || i.apiVersion !== 1) &&
+    console.error(
+      "LuoguSP CDN runtime was not initialized. Please update or reinstall LuoguSP.",
+    );
+})();
